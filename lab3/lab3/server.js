e720207a53e82455ae640b75dec80d398a03c6ca
Nikhilkrishnas <42650316+Nikhilkrishnas@users.noreply.github.com>
2018-11-01 19:37:43 -0500

    lab3

--- lab3/lab3/server.js
+++ lab3/lab3/server.js
@@ -1,10 +1,10 @@
-// required packages
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const socket = require('socket.io');
 
-// app set up
+
 var app = express();
 const port = process.env.PORT || 3000;
 var server = app.listen(port, function(){
@@ -15,14 +15,14 @@ app.use(bodyParser.json());
 const route = require('./routes/users');
 app.use('/lab-3', route);
 
-// including static files
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// socket setup
+
 var io = socket(server);
 io.on('connection', function (socket) {
     console.log("socket connection successful : " + socket.id)
